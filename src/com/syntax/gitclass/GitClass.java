3124d52ae11b2538882f84282aa81b2a225b7e01
TatiEJ <tatianajv26@gmail.com>
2022-06-01 21:31:07 -0400

    modified the git GitClass

--- src/com/syntax/gitclass/GitClass.java
+++ src/com/syntax/gitclass/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I do love you");
+		System.out.println("youuuuuu");
 	}
 
 }
