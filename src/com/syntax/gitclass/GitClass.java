05e4be112bd7fbf3a5f25ef6467b53fb3cde7333
TatiEJ <tatianajv26@gmail.com>
2022-06-01 21:20:13 -0400

    added a git gitclass

---
+++ src/com/syntax/gitclass/GitClass.java
@@ -0,0 +1,10 @@
+package com.syntax.gitclass;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I do love you");
+	}
+
+}
